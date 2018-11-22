d78e65b133c3b26a1050860028527217855b4e88
heqinghai <15088709635@163.com>
2018-11-22 21:47:07 +0800

    移除木兰路

--- InterfaceHttpExecute.java
+++
@@ -1,9 +0,0 @@
-package com.web.clients;
-
-import org.apache.http.client.methods.CloseableHttpResponse;
-
-public interface InterfaceHttpExecute {
-
-	public CloseableHttpResponse executeHttp(ParamBean paramBean);
-
-}
